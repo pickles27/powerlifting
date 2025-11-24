a8abb0c259f3ab0eeded5f7a9099d119bb1c0779
Sarah Pickrahn <spickrahn@gmail.com>
2025-11-24 13:20:43 -0800

    add debug log

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,6 +23,7 @@ async function startServer() {
 
   await server.start();
 
+  console.log("Allowed client URLs:", process.env.CLIENT_URLS);
   const clientUrls = process.env.CLIENT_URLS
     ? process.env.CLIENT_URLS.split(",").map((url) => url.trim())
     : [];
